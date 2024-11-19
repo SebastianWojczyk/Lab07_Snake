cb990270207e7b0d11fd23e9ae3c5ae77096b2b9
SebastianWojczyk <62283873+SebastianWojczyk@users.noreply.github.com>
2024-11-19 15:23:33 +0100

    Add files via upload

---
+++ Lab07_Snake/FormGameSnake.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace Lab07_Snake
+{
+    partial class FormGameSnake
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxGame = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGame)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxGame
+            // 
+            this.pictureBoxGame.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxGame.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxGame.Name = "pictureBoxGame";
+            this.pictureBoxGame.Size = new System.Drawing.Size(800, 450);
+            this.pictureBoxGame.TabIndex = 0;
+            this.pictureBoxGame.TabStop = false;
+            this.pictureBoxGame.SizeChanged += new System.EventHandler(this.PictureBoxGame_SizeChanged);
+            // 
+            // FormGameSnake
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBoxGame);
+            this.Name = "FormGameSnake";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FormGameSnake_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGame)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxGame;
+    }
+}
+
---
+++ Lab07_Snake/FormGameSnake.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Authentication.ExtendedProtection.Configuration;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab07_Snake
+{
+    public partial class FormGameSnake : Form
+    {
+        private Graphics graph;
+        private Timer timer;
+        private Random generator;
+
+        private Size boardSize;
+        private List<Point> snake;
+        private Keys direction;
+        private Point apple;
+
+        public FormGameSnake()
+        {
+            InitializeComponent();
+            generator = new Random();
+
+            boardSize = new Size(41, 31);
+
+            snake = new List<Point>();
+            snake.Add(new Point(boardSize.Width / 2, boardSize.Height - 1));
+            snake.Add(new Point(boardSize.Width / 2, boardSize.Height));
+            snake.Add(new Point(boardSize.Width / 2, boardSize.Height + 1));
+            snake.Add(new Point(boardSize.Width / 2, boardSize.Height + 2));
+            snake.Add(new Point(boardSize.Width / 2, boardSize.Height + 3));
+            direction = Keys.Up;
+
+            genApple();
+
+            PictureBoxGame_SizeChanged(null, null);
+
+            timer = new Timer();
+            timer.Interval = 100;//10 times/sec
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            Point OldHead = snake.First();
+
+            Point NewHead = Point.Empty;
+            switch (direction)
+            {
+                case Keys.Up:
+                    NewHead = new Point(OldHead.X, OldHead.Y - 1);
+                    break;
+                case Keys.Down:
+                    NewHead = new Point(OldHead.X, OldHead.Y + 1);
+                    break;
+                case Keys.Left:
+                    NewHead = new Point(OldHead.X - 1, OldHead.Y);
+                    break;
+                case Keys.Right:
+                    NewHead = new Point(OldHead.X + 1, OldHead.Y);
+                    break;
+            }
+            //game over - collision
+            if (NewHead.X < 0 || NewHead.X >= boardSize.Width || //left and right border
+                NewHead.Y < 0 || NewHead.Y >= boardSize.Height || //top and bottom border
+                snake.Contains(NewHead) //collision with snake
+               )
+            {
+                timer.Stop();
+            }
+            else
+            {
+                snake.Insert(0, NewHead);//add newHead
+                //Points - value types
+                if (NewHead == apple)
+                {
+                    genApple();
+                }
+                else
+                {
+                    snake.Remove(snake.Last());//remove oldTail
+                }
+                drawGame();
+            }
+        }
+
+        private void genApple()
+        {
+            apple = new Point(generator.Next(boardSize.Width),
+                              generator.Next(boardSize.Height));
+        }
+
+        private void drawGame()
+        {
+            SizeF fieldSize = new SizeF((float)pictureBoxGame.Width / boardSize.Width,
+                                        (float)pictureBoxGame.Height / boardSize.Height);
+
+            graph.Clear(Color.LightGreen);
+
+            for (int x = 0; x < boardSize.Width; x++)
+            {
+                for (int y = 0; y < boardSize.Height; y++)
+                {
+                    graph.DrawRectangle(new Pen(Color.Green),
+                                        x * fieldSize.Width,
+                                        y * fieldSize.Height,
+                                        fieldSize.Width,
+                                        fieldSize.Height);
+                }
+            }
+
+            foreach (Point p in snake)
+            {
+                graph.FillEllipse(new SolidBrush(Color.Brown),
+                                  p.X * fieldSize.Width,
+                                  p.Y * fieldSize.Height,
+                                  fieldSize.Width,
+                                  fieldSize.Height);
+            }
+
+            graph.FillEllipse(new SolidBrush(Color.Orange),
+                              apple.X * fieldSize.Width,
+                              apple.Y * fieldSize.Height,
+                              fieldSize.Width,
+                              fieldSize.Height);
+
+            pictureBoxGame.Refresh();
+        }
+
+        private void PictureBoxGame_SizeChanged(object sender, EventArgs e)
+        {
+            pictureBoxGame.Image = new Bitmap(pictureBoxGame.Width,
+                                              pictureBoxGame.Height);
+            graph = Graphics.FromImage(pictureBoxGame.Image);
+
+            drawGame();
+        }
+
+        private void FormGameSnake_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up ||
+                e.KeyCode == Keys.Down ||
+                e.KeyCode == Keys.Left ||
+                e.KeyCode == Keys.Right)
+            {
+                direction = e.KeyCode;
+            }
+        }
+    }
+}
